4293cde8ccc0f066cdfa271f52b4886bda2475ea
etornam anyidoho <etornamanyidoho@outlook.com>
2020-11-02 14:51:40 +0000

    comments fixes

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,12 +1,14 @@
-import React, { useState } from "react";
+import React, { Fragment, useState } from "react";
 import { Card, CardTitle, CardBody, CardImg, CardText } from "reactstrap";
 
 const DishDetail = (props) => {
   // eslint-disable-next-line no-unused-vars
-  const [selectedDish, setSelectedDish] = useState(null);
+  
+  
+  //const [selectedDish, setSelectedDish] = useState(null);
 
   const renderSelectDish = (dish) => {
-    dish = props.selectedDish;
+    //dish = props.selectedDish;
 
     if (dish != null) {
       return (
@@ -27,28 +29,51 @@ const DishDetail = (props) => {
     }
   };
 
-  const renderComment = (comments) => {
-      if (comments != null) {
-        const dcom = comments.map((comment) =>  comment['comment']);
-        console.log('dcom:', dcom);
-      }
-    comments = props.selectedDish;
+  const renderComment = (dish) => {
+    let {comments } = (dish || {});
+    console.log('dis data', comments)
+    
+    
+    //   if (dish != null) {
+    //     const dcom = comments.map((comment) =>  comments['comment']);
+    //     console.log('dcom:', dcom);
+    //   }
+    // comments = props.selectedDish;
     // const dcom = Object.keys(comments)
     
-    console.log("comment:", comments);
+    // console.log("my comment:", comments);
     if (comments instanceof Array) {
-      comments.map((comment) => {
-        return (
-          <div>
-            <ul class="list-unstyled">
-              <li>{comment.comment}</li>
-              <li>
-                {comment.author} {comment.date}
-              </li>
-            </ul>
-          </div>
-        );
-      });
+      return (
+        <Fragment>
+          {(comments || []).map((comment)=> {
+            return (
+              <div key={comment.id}>
+                <span>Hello</span>
+                <ul className="list-unstyled">
+                  <li>{comment.comment}</li>
+                  <li>
+                    {comment.author} {comment.date}
+                  </li>
+                </ul>
+              </div>
+            );
+          })}
+        </Fragment>
+      );
+      // comments.map((comment) => {
+      //   console.log('my comment', comment.author)
+      //   return (
+      //     <div>
+      //       <span>Hello</span>
+      //       {/* <ul class="list-unstyled">
+      //         <li>{comment.comment}</li>
+      //         <li>
+      //           {comment.author} {comment.date}
+      //         </li>
+      //       </ul> */}
+      //     </div>
+      //   );
+      // });
     } else {
       <div></div>;
     }
@@ -57,13 +82,15 @@ const DishDetail = (props) => {
   return (
     <div className="container">
       <div className="col-12 col-md-5 m-1">
-        {renderSelectDish(selectedDish)}
+        {renderSelectDish(props.selectedDish)}
       </div>
       <div className="col-12 col-md-5 m-1">
         <div>
           <h4>Comments</h4>
         </div>
-        <div>{renderComment(selectedDish)}</div>
+        <div>
+          {renderComment(props.selectedDish)}
+        </div>
       </div>
     </div>
   );
